c9b2b09795a3e074d8c3d786e0a179600a2d0a4f
Patrick Larsen <patrickbjerre@outlook.dk>
2023-03-09 10:58:36 +0100

    made enemy and started on bullet implementation

--- AsteroidsEntityFramework/Core/src/main/java/dk/sdu/mmmi/cbse/main/Game.java
+++ AsteroidsEntityFramework/Core/src/main/java/dk/sdu/mmmi/cbse/main/Game.java
@@ -10,6 +10,8 @@ import dk.sdu.mmmi.cbse.common.data.GameData;
 import dk.sdu.mmmi.cbse.common.data.World;
 import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
 import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
+import dk.sdu.mmmi.cbse.enemysystem.EnemyControlSystem;
+import dk.sdu.mmmi.cbse.enemysystem.EnemyPlugin;
 import dk.sdu.mmmi.cbse.managers.GameInputProcessor;
 import dk.sdu.mmmi.cbse.playersystem.PlayerControlSystem;
 import dk.sdu.mmmi.cbse.playersystem.PlayerPlugin;
@@ -43,11 +45,14 @@ public class Game
                 new GameInputProcessor(gameData)
         );
 
-        IGamePluginService playerPlugin = new PlayerPlugin();
-
+     IGamePluginService playerPlugin = new PlayerPlugin();
+        IGamePluginService enemyPlugin = new EnemyPlugin();
+        IEntityProcessingService enemyProcess = new EnemyControlSystem();
         IEntityProcessingService playerProcess = new PlayerControlSystem();
         entityPlugins.add(playerPlugin);
         entityProcessors.add(playerProcess);
+        entityPlugins.add(enemyPlugin);
+        entityProcessors.add(enemyProcess);
         // Lookup all Game Plugins using ServiceLoader
         for (IGamePluginService iGamePlugin : entityPlugins) {
             iGamePlugin.start(gameData, world);
---
+++ AsteroidsEntityFramework/Enemy/src/main/java/dk/sdu/mmmi/cbse/enemysystem/EnemyControlSystem.java
@@ -0,0 +1,77 @@
+package dk.sdu.mmmi.cbse.enemysystem;
+
+import dk.sdu.mmmi.cbse.common.data.Entity;
+import dk.sdu.mmmi.cbse.common.data.GameData;
+import dk.sdu.mmmi.cbse.common.data.World;
+import dk.sdu.mmmi.cbse.common.data.entityparts.MovingPart;
+import dk.sdu.mmmi.cbse.common.data.entityparts.PositionPart;
+import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
+
+import java.util.Random;
+
+import static dk.sdu.mmmi.cbse.common.data.GameKeys.*;
+
+public class EnemyControlSystem implements IEntityProcessingService {
+
+    @Override
+    public void process(GameData gameData, World world) {
+
+        for (Entity enemy : world.getEntities(Enemy.class)) {
+            PositionPart positionPart = enemy.getPart(PositionPart.class);
+            MovingPart movingPart = enemy.getPart(MovingPart.class);
+            Random rand = new Random();
+            int number = rand.nextInt(4);
+            movingPart.setUp(false);
+            movingPart.setRight(false);
+            movingPart.setLeft(false);
+            switch (number){
+                case 0:
+                    movingPart.setLeft(true);
+                    break;
+                case 1:
+                    movingPart.setRight(true);
+                    break;
+                case 2:
+                    movingPart.setUp(true);
+                    break;
+                case 3:
+                    //No Op for more randomness
+                    break;
+                default:
+                    System.out.println("Should not be possible");
+                    throw new IndexOutOfBoundsException();
+            }
+
+            movingPart.process(gameData, enemy);
+            positionPart.process(gameData, enemy);
+
+            updateShape(enemy);
+        }
+    }
+
+    private void updateShape(Entity entity) {
+        float[] shapex = entity.getShapeX();
+        float[] shapey = entity.getShapeY();
+        PositionPart positionPart = entity.getPart(PositionPart.class);
+        float x = positionPart.getX();
+        float y = positionPart.getY();
+        float radians = positionPart.getRadians();
+
+        shapex[0] = (float) (x + Math.cos(radians) * 5);
+        shapey[0] = (float) (y + Math.sin(radians) * 5);
+
+        shapex[1] = (float) (x + Math.cos(radians - 4 * 3.1415f / 5) * 5);
+        shapey[1] = (float) (y + Math.sin(radians - 4 * 3.1145f / 5) * 5);
+
+        shapex[2] = (float) (x + Math.cos(radians + 3.1415f) * 5);
+        shapey[2] = (float) (y + Math.sin(radians + 3.1415f) * 5);
+
+        shapex[3] = (float) (x + Math.cos(radians + 4 * 3.1415f / 5) * 5);
+        shapey[3] = (float) (y + Math.sin(radians + 4 * 3.1415f / 5) * 5);
+
+        entity.setShapeX(shapex);
+        entity.setShapeY(shapey);
+    }
+
+}
+
